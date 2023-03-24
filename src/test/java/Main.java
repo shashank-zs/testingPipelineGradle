a5703253373764842c619417c5e988372fd3420c
shashank-zs <zs@92146>
2023-03-24 17:23:17 +0530

    action

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,2 +1,12 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
 public class Main {
+    @Test
+    public static void main(){
+        System.out.println("shashank");
+        String shan ="shan1";
+        Assert.assertEquals(shan,"sha");
+    }
 }
